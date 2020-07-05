13e37ba3bcfd66db59495b4dd345311362207b81
vinay <vinay.hulgar@gmail.com>
2020-07-05 15:22:58 +0000

    Index javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Main extends React.component{
+    render() {
+        return (
+            <div className="container">
+            <h1 className="title">React Movie Search</h1>
+            </div>
+        );
+
+
+
+    }
+
+
+}
